6a841862e6ec427afa895bfb9ee7685a61db8064
mxwiser <neoway@126.com>
2023-09-21 21:31:56 +0800

    init

--- UartHelper/src/main/java/com/ybm/uart/LockController.java
+++ UartHelper/src/main/java/com/ybm/uart/LockController.java
@@ -2,16 +2,18 @@ package com.ybm.uart;
 
 public class LockController{
 
-    public static int SUCCESS =0;
-    public static int FAIL=1;
+    public static final int SUCCESS =0;
+    public static final int FAIL=1;
+    public static final int TIMEOUT=0;
+    public static final int EXPIRE=0;
 
-     private UartHelper uartHelper;
-     public LockController(UartHelper uartHelper){
+    private UartHelper uartHelper;
+    public LockController(UartHelper uartHelper){
         this.uartHelper = uartHelper;
-     }
+    }
 
     public int openAllLock(int address){
-         uartHelper.send_byte(new byte[]{(byte) 0x9D,(byte)address,(byte)0x01,(byte)0x33});
-         return SUCCESS;
+        uartHelper.send_byte(new byte[]{(byte) 0x9D,(byte)address,(byte)0x01,(byte)0x33});
+        return SUCCESS;
     }
 }
