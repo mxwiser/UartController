83c1c9deb265ad2b8c7c8c016a297790b500969c
mxwiser <neoway@126.com>
2023-09-21 19:32:22 +0800

    init

--- UartHelper/src/main/java/com/ybm/uart/UartController.java
+++ UartHelper/src/main/java/com/ybm/uart/UartController.java
@@ -3,5 +3,8 @@ package com.ybm.uart;
 import com.vi.vioserial.NormalSerial;
 
 public class UartController {
-
+   public static String Test="";
+   public UartController(){
+       NormalSerial normalSerial=new NormalSerial();
+   }
 }
