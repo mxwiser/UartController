0dddf7d0ccbb06c9397b56b36c25a3b08a28e9b0
mxwiser <neoway@126.com>
2023-09-21 20:23:56 +0800

    init

--- UartHelper/src/main/java/com/ybm/uart/UartController.java
+++ UartHelper/src/main/java/com/ybm/uart/UartController.java
@@ -1,15 +1,24 @@
 package com.ybm.uart;
 
 import com.vi.vioserial.NormalSerial;
+import com.vi.vioserial.listener.OnNormalDataListener;
 
 public class UartController {
     NormalSerial normalSerial;
     public UartController(String com,int baudRate){
         normalSerial=NormalSerial.instance();
         normalSerial.open(com,baudRate);
+        normalSerial.addDataListener(onNormalDataListener);
    }
 
    public void sendHex(String hex){
         normalSerial.sendHex(hex);
    }
+
+   OnNormalDataListener onNormalDataListener=new OnNormalDataListener() {
+       @Override
+       public void normalDataBack(String hexData) {
+
+       }
+   };
 }
